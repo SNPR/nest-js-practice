a27cb9500a219ca76f9e17095ad120c106e8c40a
Vladimir Chupin <vladimir.chupin@motorsport.com>
2021-12-20 14:02:12 +0500

    Implement custom decorator for user info receiving

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,8 +11,8 @@ import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
 import { LoginUserDto } from '@app/user/dto/login.dto';
-import { Request } from 'express';
-import { ExpressRequestInterface } from '@app/types/expressRequest.interface';
+import { User } from '@app/user/decorators/user.decorator';
+import { UserEntity } from '@app/user/user.entity';
 
 @Controller()
 export class UserController {
@@ -38,9 +38,7 @@ export class UserController {
   }
 
   @Get('/user')
-  async currentUser(
-    @Req() request: ExpressRequestInterface,
-  ): Promise<UserResponseInterface> {
-    return this.userService.buildUserResponse(request.user);
+  async currentUser(@User() user: UserEntity): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(user);
   }
 }
