97d509d2c3650fca44351056174e1a75b16aeac9
Vladimir Chupin <vladimir.chupin@motorsport.com>
2021-12-21 14:56:55 +0500

    Implement auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,7 @@ import {
   Controller,
   Get,
   Post,
-  Req,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -13,6 +13,7 @@ import { UserResponseInterface } from '@app/user/types/userResponse.interface';
 import { LoginUserDto } from '@app/user/dto/login.dto';
 import { User } from '@app/user/decorators/user.decorator';
 import { UserEntity } from '@app/user/user.entity';
+import { AuthGuard } from '@app/user/guards/auth.guard';
 
 @Controller()
 export class UserController {
@@ -38,6 +39,7 @@ export class UserController {
   }
 
   @Get('/user')
+  @UseGuards(AuthGuard)
   async currentUser(@User() user: UserEntity): Promise<UserResponseInterface> {
     return this.userService.buildUserResponse(user);
   }
